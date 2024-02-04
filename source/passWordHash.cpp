0e85f11c079b5576122507262af6aa05f1bc02d3
JFSLopes <josefilipe3333@gmail.com>
2024-02-04 13:49:59 +0000

    Populate the DB and created a simple function to encrypt the passwords

---
+++ source/passWordHash.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+namespace password
+{
+    std::string hashPassword(const std::string &password)
+    {
+        const int shift = 7;
+        std::string hashedPassword = password;
+        for (char &ch : hashedPassword)
+        {
+            ch += shift;
+        }
+        return hashedPassword;
+    }
+
+    bool verifyPassword(const std::string &password, const std::string &hashedPassword)
+    {
+        const int shift = 7;
+        std::string originalPassword = hashedPassword;
+        for (char &ch : originalPassword)
+        {
+            ch -= shift;
+        }
+        return originalPassword == password;
+    }
+}
